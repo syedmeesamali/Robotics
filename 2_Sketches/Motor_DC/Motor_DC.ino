32ad4679f28aada4d1d596b0e23690dc42c2fc26
syedmeesamali <syedmeesamali@yahoo.com>
2020-08-07 07:25:25 +0400

    Struggle to drive the DC motor

---
+++ 2_Sketches/Motor_DC/Motor_DC.ino
@@ -0,0 +1,12 @@
+int motorPin = 3;
+int ledPin = 9;
+
+//Main setup loop
+void setup () {
+  
+} //End of SETUP
+
+void loop () {
+  pinMode(motorPin, HIGH);
+  pinMode(ledPin, HIGH);
+}//End of LOOP
