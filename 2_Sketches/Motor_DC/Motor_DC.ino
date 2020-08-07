10a046ef7ef6e9a2b4ebfad03071b49f0b17699a
syedmeesamali <syedmeesamali@yahoo.com>
2020-08-07 22:55:31 +0400

    Pins simulating properly now - first make pinmode then make them high or low

--- 2_Sketches/Motor_DC/Motor_DC.ino
+++ 2_Sketches/Motor_DC/Motor_DC.ino
@@ -1,12 +1,16 @@
 int motorPin = 3;
-int ledPin = 9;
+int ledPin = 8;
+int Pin2 = 9;
 
 //Main setup loop
 void setup () {
-  
+  pinMode(ledPin, OUTPUT);
+  pinMode(motorPin, OUTPUT);
+  pinMode(Pin2, OUTPUT);
 } //End of SETUP
 
 void loop () {
-  pinMode(motorPin, HIGH);
-  pinMode(ledPin, HIGH);
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(motorPin, HIGH);
+  digitalWrite(Pin2, HIGH);
 }//End of LOOP
