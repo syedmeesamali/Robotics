90822fe27a1fc32f32a16f5f3ff8c6c9ff870005
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-05 05:00:01 +0400

    Some more servo and circuit tinkering

--- 2_Sketches/Servo_First/Servo_First.ino
+++ 2_Sketches/Servo_First/Servo_First.ino
@@ -1,15 +1,20 @@
 #include<Servo.h>   //Servo related code library
 
+#define laser 8
 Servo servo1;
 int pos = 0;      //Servo position
 
+
+
 void setup () {
+  pinMode(laser, OUTPUT);
   servo1.attach(5);     //Servo connected at pin # 5
   servo1.write(180);    //Move to 180 degrees
-  
 }
 
 void loop () {
+  digitalWrite (laser, HIGH);
+  
   for (pos = 0; pos < 180; pos++) 
   {
     servo1.write(pos);
