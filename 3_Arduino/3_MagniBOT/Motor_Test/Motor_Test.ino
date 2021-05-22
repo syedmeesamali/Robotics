afe62b250618543feaf8df29221342f81d02bcc5
syedmeesamali <syedmeesamali@yahoo.com>
2021-05-23 03:43:55 +0400

    Update to some design elements of the MagniBOT and need to re-print

--- 3_Arduino/3_MagniBOT/Motor_Test/Motor_Test.ino
+++ 3_Arduino/3_MagniBOT/Motor_Test/Motor_Test.ino
@@ -2,22 +2,25 @@
 
 int motor_1 = 8;
 Servo Motor1;
-
+int pos = 0;
 
 //Main code for setup
 void setup() {
   Motor1.attach(motor_1);
 }
 
+//Loop which will run continuously over the course of program
 void loop() {
-  Motor1.write(0);
-  delay(500);
-  Motor1.write(45);
-  delay(500);
-  Motor1.write(135);
-  delay(500);
-  Motor1.write(180);
-  delay(500);
-  Motor1.write(0);
-  delay(500);
+  for (pos = 0; pos < 180; pos++)
+  {
+    Motor1.write(pos);
+    delay(15);
+  }
+  delay(1500);
+  for (pos = 180; pos > 0; pos--)
+  {
+    Motor1.write(pos);
+    delay(15);
+  }
+  delay(1500);
 }
