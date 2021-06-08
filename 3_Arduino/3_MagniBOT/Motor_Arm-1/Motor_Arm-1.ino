b309da187c6bdc58b6834a41f3d57a0a6148820f
syedmeesamali <syedmeesamali@yahoo.com>
2021-06-08 23:45:02 +0400

    Independently running motors using two separate pins via expansion board for Nano

--- 3_Arduino/3_MagniBOT/Motor_Arm-1/Motor_Arm-1.ino
+++ 3_Arduino/3_MagniBOT/Motor_Arm-1/Motor_Arm-1.ino
@@ -1,12 +1,15 @@
 #include<Servo.h>
 
 int motor_1 = 7;
+int motor_2 = 8;
 Servo Motor1;
+Servo Motor2;
 int pos = 0;
 
 //Main code for setup
 void setup() {
   Motor1.attach(motor_1);
+  Motor2.attach(motor_2);
 }
 
 //Loop which will run continuously over the course of program
@@ -15,11 +18,15 @@ void loop() {
   {
     Motor1.write(pos);
     delay(50);
+    Motor2.write(pos);
+    delay(50);
   }
   delay(2000);
   for (pos = 160; pos > 50; pos--)
   {
     Motor1.write(pos);
+    delay(50);
+    Motor2.write(pos);
     delay(50);
   }
   delay(2000);
