6c3c425622d64733f7c5f65e90a9cee1498641e9
syedmeesamali <syedmeesamali@yahoo.com>
2021-09-26 03:31:30 +0400

    Some review done for the gripper code and played on actual - very clumsy but somewhat works

--- 3_Arduino/3_MagniBOT/ino/motor_move_LDR/motor_move_LDR.ino
+++ 3_Arduino/3_MagniBOT/ino/motor_move_LDR/motor_move_LDR.ino
@@ -1,12 +1,10 @@
 #include<Servo.h>
 
 //LDR Input will be used as a reference
-int grip_hand = 4;
+int grip_hand = 7;
 
 //Declare a total of six (06) no's of motors - SG90 Servo or MG996R Servo
 Servo grip_hand_motor;
-Servo grip_base_motor;
-Servo grip_arm_motor;
 
 int val;            //Value to be received from Processing via serial port
 int pos = 0;        //Motor position value
@@ -14,26 +12,21 @@ int pos = 0;        //Motor position value
 //Main code for setup - Only motors are connected to pins here
 void setup() {
   grip_hand_motor.attach(grip_hand);
-  grip_base_motor.attach(grip_base);
-  grip_arm_motor.attach(grip_arm);
 }
 
-//Poll the serial port for signal and then move motors accordingly
 void loop() {
   //Rotation of motor in one direction
-  for (pos = 0; pos <= 180; pos++)
+  for (pos = 90; pos <= 120; pos+=5)
   {
-    grip_arm_motor.write(pos);
-    if (pos >= 90 && pos <=120)     //Move second motor only half way
-    {
-      if (pos >= 65 && pos <=100)   //Move third motor even lesser quantity
-      {
-        grip_base_motor.write(pos);
-      }
-      grip_hand_motor.write(pos);
-    }
-    delay(50);
+    grip_hand_motor.write(pos);
+    delay(20);
   }
- 
+  delay(1500);
+  //Rotation of motor in other direction
+  for (pos = 120; pos >= 90; pos-=5)
+  {
+    grip_hand_motor.write(pos);
+    delay(20);
+  }
   delay(1500);
 }
