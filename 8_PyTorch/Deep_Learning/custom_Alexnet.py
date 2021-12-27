8a280e31d2dbc89c461e17fefcd7b29188584ac0
syedmeesamali <syedmeesamali@yahoo.com>
2021-12-27 16:59:30 +0400

    AlexNet classification for the images - some small kinks to be still removed

---
+++ 8_PyTorch/Deep_Learning/custom_Alexnet.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import torch
+import os
+from torchvision import transforms, models
+img = Image.open(r'D:/repos/Robotics/8_PyTorch/Deep_Learning/tiger.jpg')
+print(img)
+
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean = [0.485, 0.456, 0.406],
+        std = [0.229, 0.224, 0.225]
+    )
+])
+
+img_tensor = transform(img)
+batch = img_tensor.unsqueeze(0)
+model = models.alexnet(pretrained=True)
+
+device = "cpu"
+model.eval()
+model.to(device)
+y = model(batch.to(device))
+
+prob = torch.nn.functional.softmax(y, dim=1)[0] * 100
+_, indices = torch.sort(y, descending=True)
+for idx in indices[0][:5]:
+    print(classes[idx], prob[idx].item())
--- 8_PyTorch/coffee_NN.py
+++ 8_PyTorch/coffee_NN.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 img = Image.open('coffee.jpg')
-plt.imshow(img)
-plt.show()
+#plt.imshow(img)
+#plt.show()
 
 import torch
 from torchvision import transforms
-
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -19,4 +18,26 @@ transform = transforms.Compose([
 ])
 
 img_tensor = transform(img)
-print(type(img_tensor), img_tensor.shape)
+batch = img_tensor.unsqueeze(0)
+
+from torchvision import models
+import urllib
+#Alexnet is most famous image classification model and we will use pre-trained model
+
+model = models.alexnet(pretrained=True)
+
+device = "cpu"
+model.eval()
+model.to(device)
+y = model(batch.to(device))
+print(y.shape)
+y_max, index = torch.max(y, 1)
+print(index, y_max)
+
+url = 'https://pytorch.tips/imagenet-labels'
+fpath = 'imagenet_class_labels.txt'
+urllib.request.urlretrieve(url, fpath)
+with open ('imagenet_class_labels.txt') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+print(classes[967])
