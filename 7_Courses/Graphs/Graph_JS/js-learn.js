821321965040c975e6017666356abd138c878578
syedmeesamali <syedmeesamali@yahoo.com>
2021-12-30 12:33:54 +0400

    JS udacity course on objects and various methods

---
+++ 7_Courses/Graphs/Graph_JS/js-learn.js
@@ -0,0 +1,44 @@
+//Define transaction
+const menu = {
+    name: 'Salted Caramel Ice Cream',
+    price: 2.95,
+    ingredients: ['butter', 'ice cream', 'salt', 'sugar']
+  }
+
+
+document.write('Ingredients: ' + menu.ingredients + '</br>');
+
+const bicycle = {
+    color: 'blue',
+    type: 'mountain bike',
+    wheels: {
+      diameter: 18,
+      width: 8
+    }
+  };
+
+document.write('Bicyle wheel dia: ' + bicycle.wheels.diameter + '</br>');
+
+const greetings = {
+    hello: {
+      english: 'hi',
+      french: 'bonjour',
+      portuguese: 'oi'
+    },
+    goodbye: {
+      english: 'bye',
+      french: 'au revoir',
+      portuguese: 'tchau'
+    }
+  };
+
+  document.write('Hello in portugese: ' + greetings.hello.portuguese + '</br>');
+  let originalObject = {
+    favoriteColor: 'red'
+  };
+  function setToBlue(object) {
+    object.favoriteColor = 'blue';
+  }
+  
+  setToBlue(originalObject);
+  document.write('Fav color: ' + originalObject.favoriteColor + '</br>');
